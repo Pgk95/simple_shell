1e06bbfa7ae589aff6b1d1bb85a5a2e26e2954d9
pgk95 <kalupgk@gmail.com>
2023-02-22 04:09:07 -0800

    task number 1

---
+++ cmdline.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - stores arguments.
+ * Return: Always (0).
+ */
+int main(void)
+{
+	char *command = NULL;
+	size_t len = 0;
+
+	while (1)
+	{
+		printf("$ ");
+		getline(&command, &len, stdin);
+		printf("%s", command);
+	}
+	free(command);
+	return (0);
+}
